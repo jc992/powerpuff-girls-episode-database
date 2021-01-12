5514f27209784f6a5d3ffac50c5bab397dfe2805
JoaoT <joaot@agap2.pt>
2021-01-12 17:02:11 -0100

    added comment for useEffect on EpisodeDetailsPage, and tweaked some styling on NotFound Component

--- src/pages/EpisodeDetailsPage.js
+++ src/pages/EpisodeDetailsPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import { EpisodeDetailContainer } from '../components/episode/episode-detail/EpisodeDetailContainer';
 import { Context } from '../context/Context';
 import { EnhancedClipLoader } from '../utils/utils';
@@ -6,17 +6,30 @@ import { EnhancedClipLoader } from '../utils/utils';
 /**
  * Returns specific episode details page.
  */
-const EpisodeDetailsPage = ({ match }) => {
+const EpisodeDetailsPage = ({ match, history }) => {
     const context = useContext(Context);
     const { episodeList } = context;
     const { number, season } = match.params;
+
+    let episode;
+
+    /**
+     * This guarantees that if you don't get stuck waiting for the page to render if you try to access a nonexistent episode.
+     */
+    useEffect(() => {
+        setTimeout(() => {
+            if (episode === undefined) {
+                history.push('/NotFound');
+            }
+        }, 5000)
+    }, [history, episode]);
 
     if (episodeList === undefined) return <EnhancedClipLoader />;
 
     /**
      * This variable filters out the episode matching the accessed navigation link by number and season.
      */
-    let episode = episodeList.filter(episode => episode.season === parseInt(season) && episode.number === parseInt(number))[0]
+    episode = episodeList.filter(episode => episode.season === parseInt(season) && episode.number === parseInt(number))[0]
 
     return (
         <Fragment>
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { NOT_FOUND_GIF } from '../utils/utils';
 
 export const NotFound = () => {
     return (
-        <Fragment>
-            <img src={NOT_FOUND_GIF} alt="Invalid Route" style={{ borderRadius: '10px' }}></img>
+        <div className="notfound-container">
+            <img src={NOT_FOUND_GIF} alt="Invalid Route"></img>
             <h1>404 Page Not Found</h1>
-        </Fragment>
+        </div>
     );
 };
 
